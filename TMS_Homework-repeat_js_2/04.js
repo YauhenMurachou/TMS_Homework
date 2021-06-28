07f01d3061cebaad874eec94bf01a0d3a4399ad0
YauhenMurachou <emurach1992@gmail.com>
2021-06-29 00:01:42 +0300

    Tadk 4 corrected

--- TMS_Homework-repeat_js_2/04.js
+++ TMS_Homework-repeat_js_2/04.js
@@ -1,5 +1,9 @@
 // - Посчитать с помощью рекурсии факториал числа 10. Например: factorial(n); (При вызове полжите n = 10);
 
 function factorial(n) {
-	return (n != 1) ? n * factorial(n - 1) : 1;
+	if (n <= 1) {
+		return 1
+	} else {
+		return n * factorial(n - 1);
+	}
 }
