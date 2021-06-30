3522b228e0fa740e95b439ceb33c656cb6ddeb9e
YauhenMurachou <emurach1992@gmail.com>
2021-06-30 14:42:11 +0300

    Task 6 correctea

--- TMS_Homework-repeat_js_2/06.js
+++ TMS_Homework-repeat_js_2/06.js
@@ -2,7 +2,6 @@
 // 	  С помощью данной функции создайте два объекта пользователей (два пользователя), имя, фамилию, возраст, город проживания задайте
 // 	  сами. 
 
-
 function User(first_name, second_name, age, city) {
 	this.first_name = first_name;
 	this.second_name = second_name;
@@ -14,21 +13,20 @@ let belarusian = new User('Андрей', 'Станкевич', 22, 'Минск'
 
 let ukrainian = new User('Дмитро', 'Гордон', 52, 'Киев');
 
-
 // - Создайте функцию, которая позовляет вывести полную информацию о каждом из двух пользователей созданных в предыдущем задании.
 // 	  Функция должна выполнять вывод информации и не принимать аргументов. По сути нужно одну функцию привязать два объекта. 
 
-
-function info() {
-
-	let belObject = {
-		__proto__: belarusian,
-	}
-
+User.prototype.info = function() {
 	console.log(this.first_name, this.second_name, this.age, this.city)
-	console.log(belObject.first_name, belObject.second_name, belObject.age, belObject.city)
 }
 
-let infoNation = info.bind(ukrainian);
+belarusian.info()
 
-infoNation()
+ukrainian.info()
+
+
+
+
+
+
+
