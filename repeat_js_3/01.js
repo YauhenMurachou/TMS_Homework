3c7caf6b6d132ec0353d0161696319c2e446212a
YauhenMurachou <77886950+YauhenMurachou@users.noreply.github.com>
2021-07-15 10:30:48 +0300

    .

--- repeat_js_3/01.js
+++
@@ -1,8 +0,0 @@
-// - Дан объект let user = { name: "Ivan", years: 19, car: "Subaru", telePhone: "iPhone", city: "Minsk" }.
-// 	  Запишите поле name в переменную userName, поле year в переменную userName, поле car в переменную userCar, остальные поля
-//   	  записать в объект userOtherInfo;
-
-
-let user = { name: "Ivan", years: 19, car: "Subaru", telePhone: "iPhone", city: "Minsk" };
-
-let { name: userName, years: userYears, car: userCar, ...userOtherInfo } = user;
