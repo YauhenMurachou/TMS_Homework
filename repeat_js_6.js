171b9f5a2eacf2ca508e927f0d19621d4f45154d
YauhenMurachou <emurach1992@gmail.com>
2021-07-21 15:12:16 +0300

    HW6

---
+++ repeat_js_6.js
@@ -0,0 +1,37 @@
+// - Перепишите цепочку промисов из предыдущего задания, используя async/await
+
+
+function cleanRoom(dirtyLevel) {
+
+	if (typeof (dirtyLevel) !== 'number' || dirtyLevel < 0 || isNaN(dirtyLevel)) {
+		alert('Нужно писать положительное число!');
+	} else {
+
+		return new Promise(function (resolve, reject) {
+			if (dirtyLevel <= 10) {
+				setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
+			} else {
+				setTimeout(() => reject('Слишком грязно'), dirtyLevel * 1000);
+			}
+		});
+	}
+};
+
+
+async function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+	try {
+		let firstResult = await cleanRoom(dirtyLevel_1);
+		console.log(`Уборка первой комнаты проведена успешно за ${firstResult} секунд`);
+
+		let secondResult = await cleanRoom(dirtyLevel_2);
+		console.log(`Уборка второй комнаты проведена успешно за ${secondResult} секунд`);
+
+		let thirdResult = await cleanRoom(dirtyLevel_3);
+		console.log(`Уборка третьей комнаты проведена успешно за ${thirdResult} секунд`);
+
+	} catch (err) {
+		console.error(err);
+	};
+}
+
+cleanRooms(3, 2, 5);
